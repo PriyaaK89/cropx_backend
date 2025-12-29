92dbd81cf06c4ddd61341a9b55daa224e6bf2e0f
kumawatPriya <priyakumawat9868@gmail.com>
2025-12-29 17:07:12 +0530

    implement checks in razorpay

--- src/service/razorpay.js
+++ src/service/razorpay.js
@@ -1,8 +1,14 @@
 const Razorpay = require("razorpay");
 
+if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+  throw new Error(
+    "Razorpay keys missing. Make sure environment variables are set in Railway."
+  );
+}
+
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 module.exports = razorpay;
