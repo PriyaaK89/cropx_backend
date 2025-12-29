91094c14c187c423882d7f96d611eaec96f7bd20
kumawatPriya <priyakumawat9868@gmail.com>
2025-12-29 17:34:54 +0530

    fixes

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,32 @@
 const mysql = require("mysql2/promise");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 // dotenv.config();
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,         
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT || 3306),   
+  host: process.env.MYSQL_HOST,      // Railway MySQL host
+  user: process.env.MYSQL_USER,      // Railway MySQL user
+  password: process.env.MYSQL_PASSWORD, // Railway MySQL password
+  database: process.env.MYSQL_DATABASE, // Railway MySQL database name
+  port: process.env.MYSQL_PORT,      // Railway MySQL port (usually 3306)
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 module.exports = db;
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,         
+//   user: process.env.DB_USER ,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: Number(process.env.DB_PORT || 3306),   
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = db;
