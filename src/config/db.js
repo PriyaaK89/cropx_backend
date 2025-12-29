a980cc08c4112607378231b1ec4a731f10bf328f
kumawatPriya <priyakumawat9868@gmail.com>
2025-12-29 16:21:30 +0530

    fixes

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "mysql",         
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "sqlroot1238",
-  database: process.env.DB_NAME || "cropx_db",
-  port: process.env.DB_PORT || 3306,   
+  host: process.env.DB_HOST,         
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 3306),   
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
