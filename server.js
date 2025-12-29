ac5329d0ad855086c37a7f6aec2506171f9fad51
kumawatPriya <priyakumawat9868@gmail.com>
2025-12-29 17:37:47 +0530

    fixed db file

--- server.js
+++ server.js
@@ -78,6 +78,6 @@ app.get("/", (req, res) => {
   res.send("CropX backend is running");
 });
 
-// Start server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
